9de5e48d7a6ad4efa599aba42fbaee77907cd900
Undefined2624 <111666919+Undefined2624@users.noreply.github.com>
2024-02-13 21:00:23 -0300

    Se agregó clase BL_Usuarios y método que retorna usuarios del metodo de la capa de datos

---
+++ BusinessLayer/BL_Usuarios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer;
+using DataLayer;
+
+namespace BusinessLayer
+{
+    public class BL_Usuarios
+    {
+        private DL_Usuarios oDL_Usuarios = new DL_Usuarios();
+
+        public List<Usuario> ListarUsuarios()
+        {
+            return oDL_Usuarios.listarUsuarios();
+        }
+
+    }
+}
